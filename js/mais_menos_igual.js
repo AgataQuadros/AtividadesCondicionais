34855075d9458eeeb14e7e13066ac90948730851
AgataQuadros <quadrosagata@gmail>
2025-06-27 19:17:53 -0300

    Maior, Menor ou Iguais

--- js/mais_menos_igual.js
+++ js/mais_menos_igual.js
@@ -1,24 +1,24 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     const saida = document.getElementById('resposta')
     const entrada1 = document.getElementById('entrada1')
     const entrada2 = document.getElementById('entrada2')
     const entrada3 = document.getElementById('entrada3')
 
 
-    document.getElementById('btn').addEventListener('click', function(){
+    document.getElementById('btn').addEventListener('click', function () {
 
-        if(entrada1.value<entrada2.value<entrada3.value){
-            saida.textContent = `{${entrada3},${entrada2},${entrada1}}`
+        if (entrada1.value > entrada2.value && entrada1 > entrada3.value) {
+            saida.textContent = `${entrada1.value} é o maior`
         }
-        else if(entrada2.value<entrada3.value<entrada1.value){
-            saida.textContent = `{${entrada1},${entrada2},${entrada3}}`
+        else if (entrada2.value > entrada3.value && entrada2.value > entrada1.value) {
+            saida.textContent = `${entrada2.value} é o maior`
         }
-        else if(entrada3.value<entrada1.value<entrada2.value){
-            saida.textContent = `{${entrada2},${entrada1},${entrada3}}`
+        else if(entrada1.value === entrada2.value && entrada2.value === entrada3.value){
+            saida.textContent = "Todos os valores são iguais"
         }
-        else if(entrada3.value<entrada1.value<entrada2.value){
-            saida.textContent = `{${entrada2},${entrada1},${entrada3}}`
+        else {
+            saida.textContent = `${entrada3.value} é o maior`
         }
     });
-    
+
 });
