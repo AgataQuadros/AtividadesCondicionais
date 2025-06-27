89a8a4d0e383e41fad23955acc89ea952f070a70
AgataQuadros <quadrosagata@gmail>
2025-06-26 21:33:20 -0300

    Terminar

---
+++ js/mais_menos_igual.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const entrada1 = document.getElementById('entrada1')
+    const entrada2 = document.getElementById('entrada2')
+    const entrada3 = document.getElementById('entrada3')
+
+    
+});
