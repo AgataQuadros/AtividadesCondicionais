93d77cbcec77052e6a65db26040dd57a76cc8311
AgataQuadros <quadrosagata@gmail>
2025-06-27 16:35:27 -0300

    testar com o computador do curso pq o meu é meia merda

--- js/mais_menos_igual.js
+++ js/mais_menos_igual.js
@@ -1,7 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const saida = document.getElementById('resposta')
     const entrada1 = document.getElementById('entrada1')
     const entrada2 = document.getElementById('entrada2')
     const entrada3 = document.getElementById('entrada3')
 
+
+    document.getElementById('btn').addEventListener('click', function(){
+
+        if(entrada1.value<entrada2.value<entrada3.value){
+            saida.textContent = `{${entrada3},${entrada2},${entrada1}}`
+        }
+        else if(entrada2.value<entrada3.value<entrada1.value){
+            saida.textContent = `{${entrada1},${entrada2},${entrada3}}`
+        }
+        else if(entrada3.value<entrada1.value<entrada2.value){
+            saida.textContent = `{${entrada2},${entrada1},${entrada3}}`
+        }
+        else if(entrada3.value<entrada1.value<entrada2.value){
+            saida.textContent = `{${entrada2},${entrada1},${entrada3}}`
+        }
+    });
     
 });
